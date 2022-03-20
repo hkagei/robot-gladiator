a547e22a7f45288baca36512b7789a68cb87a203
hkagei <k_dawg_1022@hotmail.com>
2022-03-20 17:03:42 -0600

    add enemy combatants

--- projects/robot-gladiator/assets/js/game.js
+++ projects/robot-gladiator/assets/js/game.js
@@ -5,11 +5,19 @@ var playerAttack = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+console.log(enemyNames.length);
+for(var i = 0; i < 3; i++) {
+  console.log(enemyNames[i]);
+  console.log(i);
+  console.log(enemyNames[i] + " is at " + i + " index");
+  }
+
+var fight = function(enemyName) {
+    ...// fight function statements
 var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 // if player choses to fight, then fight
 if (promptFight === "fight" || promptFight === "FIGHT") {
@@ -54,6 +62,9 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
       fight();
     }
   }
+  
     };
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+        fight(enemyNames[i]);
+      }
