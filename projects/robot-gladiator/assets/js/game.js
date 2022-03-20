99d408697a76547042d066853954bd66fd8ef12d
hkagei <k_dawg_1022@hotmail.com>
2022-03-20 15:18:50 -0600

    update jss

--- projects/robot-gladiator/assets/js/game.js
+++ projects/robot-gladiator/assets/js/game.js
@@ -2,7 +2,8 @@
 var playername = window.prompt("What is your robot's name?");
 // note the lack of quotation marks around playername
 window.alert(playername);
-function // fight(){
-    window.alert("The fight has begun!")
+function fight(){
+    window.alert("The fight has begun!");
+    console.log(playername);
 }
 fight()
