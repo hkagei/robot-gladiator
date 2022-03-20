61bd8e005f8a4b2e324eee5e8244f9e3fa042a7a
hkagei <k_dawg_1022@hotmail.com>
2022-03-20 15:28:22 -0600

    further .js changes

--- projects/robot-gladiator/assets/js/game.js
+++ projects/robot-gladiator/assets/js/game.js
@@ -5,5 +5,12 @@ window.alert(playername);
 function fight(){
     window.alert("The fight has begun!");
     console.log(playername);
+    console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+    console.log(10 + 10);
+// what is this?
+    console.log("Our robot's name is " + playername);
 }
 fight()
+var name = "your name";
+console.log(name);
