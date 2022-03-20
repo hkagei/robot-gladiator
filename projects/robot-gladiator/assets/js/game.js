80ccab07e6296b99597a8a4f3cb1751b0023ae27
hkagei <k_dawg_1022@hotmail.com>
2022-03-20 15:00:27 -0600

    initialize robot gladiator

--- projects/robot-gladiator/assets/js/game.js
+++ projects/robot-gladiator/assets/js/game.js
@@ -1,1 +1,8 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+var playername = window.prompt("What is your robot's name?");
+// note the lack of quotation marks around playername
+window.alert(playername);
+function // fight(){
+    window.alert("The fight has begun!")
+}
+fight()
